aa2ad933b52447c3a8d4577027991f02832597ac
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 22:45:15 +0530

    Update read_write_decision.v

--- read_write_decision.v
+++ read_write_decision.v
@@ -1,6 +1,9 @@
+// Verilog code for decision of read-write operation
 module read_write_decision(write_mode, read_mode, out_enable, chip_select, wire_enable);
-	output write_mode, read_mode;
+	// inputs and outputs ports declaration
 	input out_enable, chip_select, wire_enable;
+	output write_mode, read_mode;
+	// code start
 	assign read_mode = out_enable & chip_select;
 	assign write_mode = wire_enable & chip_select;
-endmodule
+endmodule // end module of read write decision
