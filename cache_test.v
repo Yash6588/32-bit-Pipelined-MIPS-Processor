c762f59f3fbf1a3037989b0c3fbb48f2827a9952
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 23:51:35 +0530

    Delete cache_test.v

--- cache_test.v
+++
@@ -1,8 +0,0 @@
-module cache_test(out[31:0], address[31:0], write_signal, read_signal);
-	input [31:0] address;
-	input write_signal, read_signal;
-	output [31:0] out;
-	wire [15:0] tag;
-	wire [11:0] line_index;
-	wire [3:0] byte_offset;
-endmodule
