26510efc4ce1d04acbd0a4fc5c7816733aaebbae
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 11:18:43 +0530

    comment update

--- mux4x1.v
+++ mux4x1.v
@@ -1,6 +1,9 @@
+// Verilog code for 4X1 mux 
 module mux4x1(out[31:0], in0[31:0], in1[31:0], in2[31:0], in3[31:0], sel[1:0]);
-	output [31:0] out;
+	// inputs and outputs ports declaration
 	input [31:0] in0, in1, in2, in3;
 	input [1:0] sel;
+	output [31:0] out;
+	// code start
 	assign out = sel[1] ? (sel[0] ? in3 : in2) : (sel[0] ? in1 : in0);
-endmodule
+endmodule // end of module mux
