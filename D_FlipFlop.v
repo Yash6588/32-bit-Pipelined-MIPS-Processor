2ea95b0e99e262454625479d26e6697032567358
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 10:33:26 +0530

    Update D_FlipFlop.v

--- D_FlipFlop.v
+++ D_FlipFlop.v
@@ -1,14 +1,16 @@
+// Verilog code for D Flip Flop
 module D_FlipFlop(q,q1,d,c);
-output reg q,q1;
- input d,c;
-  
+	//inputs and outputs variables declaration
+	output reg q,q1;
+	input d,c;
 	initial 
-	   begin
+	begin
 		   q=1'b0; q1=1'b0;
-	  end
+	end
+	//Always block with inputs in the sensitivity list.
 	always @ (posedge c)
-	   begin 
+	begin 
 		 q=d;
 		 q1= ~d;
-	   end
+	end
 endmodule
