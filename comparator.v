bf79f7ba361784bd19659319eadac3206b193641
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 09:24:18 +0530

    comparator

---
+++ comparator.v
@@ -0,0 +1,5 @@
+module comparator(comp_out, in1[4:0], in2[4:0]);
+	input [4:0] in1, in2;
+	output comp_out;
+	assign comp_out = ~((in1[4] ^ in2[4]) | (in1[3] ^ in2[3]) | (in1[2] ^ in2[2]) | (in1[1] ^ in2[1]) | (in1[0] ^ in2[0]));
+endmodule
