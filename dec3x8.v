da33898341f108144da46504246a56307266a355
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 23:39:12 +0530

    Update dec3x8.v

--- dec3x8.v
+++ dec3x8.v
@@ -1,7 +1,10 @@
+// Verilog code for 3X8 decoder
 module dec3x8(out[7:0], sel[2:0], enable);
+	// input and output ports declaration
 	input [2:0] sel;
 	input enable;
 	output [7:0] out;
+	// code start
 	assign out[0] = ~sel[2] & ~sel[1] & ~sel[0] & enable;
 	assign out[1] = ~sel[2] & ~sel[1] & sel[0] & enable;
 	assign out[2] = ~sel[2] & sel[1] & ~sel[0] & enable;
@@ -10,4 +13,4 @@ module dec3x8(out[7:0], sel[2:0], enable);
 	assign out[5] = sel[2] & ~sel[1] & sel[0] & enable;
 	assign out[6] = sel[2] & sel[1] & ~sel[0] & enable;
 	assign out[7] = sel[2] & sel[1] & sel[0] & enable;
-endmodule
+endmodule	// end of module decoder
