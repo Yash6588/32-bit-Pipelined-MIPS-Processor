67a5e9c20c2c5d3d1b6e3ea53f959a877b8395ed
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 10:50:15 +0530

    comment update

--- logic_unit.v
+++ logic_unit.v
@@ -1,6 +1,9 @@
+// Verilog code for Logic Unit
 module logic_unit(and_out[31:0], or_out[31:0], xor_out[31:0], nor_out[31:0], x[31:0], y[31:0]);
+	// inputs and outputs variables declaration
 	input [31:0] x,y;
 	output [31:0] and_out, or_out, xor_out, nor_out;
+	// Logical Operation between x and y
 	assign and_out = x & y;
 	assign or_out = x | y;
 	assign xor_out = x ^ y;
