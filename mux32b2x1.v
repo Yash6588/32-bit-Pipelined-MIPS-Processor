6477937bb7ff207e4b3284a5e8090b00ffa4b0c9
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 23:16:22 +0530

    Update mux32b2x1.v

--- mux32b2x1.v
+++ mux32b2x1.v
@@ -1,6 +1,9 @@
+// Verilog code for 32 bit Mux
 module mux32b2x1(out[31:0], in0[31:0], in1[31:0], select);
+	// inputs and output ports declaration
 	input [31:0] in0,in1;
-	output [31:0] out;
 	input select;
+	output [31:0] out;
+	// code start
 	assign out = select ? in1 : in0;
-endmodule
+endmodule	// end of module Mux
