31c7d57e400d1c73bcc27bb09e30572e8397bf7a
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 22:22:16 +0530

    Update shifter.v

--- shifter.v
+++ shifter.v
@@ -1,9 +1,12 @@
+// Verilog code for shifter
 module shifter(no_shift[31:0], logic_left[31:0], logic_right[31:0], arith_right[31:0], in[31:0], shift_amt[4:0]);
+	// inputs and outputs ports declaration
 	input [31:0] in;
-	output [31:0] no_shift, logic_left, logic_right, arith_right;
 	input [4:0] shift_amt;
+	output [31:0] no_shift, logic_left, logic_right, arith_right;
+	// code start
 	assign no_shift = in;
 	assign logic_left = (in << shift_amt);
 	assign logic_right = (in >> shift_amt);
 	assign arith_right = ($signed(in) >>> shift_amt);
-endmodule
+endmodule  // end of module shifter
