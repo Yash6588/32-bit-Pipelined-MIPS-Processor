4921917d41d5719e0efe4e9f41600deedf3e6abc
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 23:23:25 +0530

    Update delay.v

--- delay.v
+++ delay.v
@@ -1,7 +1,9 @@
 `timescale 1ns/1ps
-
+// Verilog code for delay
 module delay(out[4:0], in[4:0]);
+	// inputs and outputs ports declaration
 	input [4:0] in;
 	output [4:0] out;
+	// code start
 	assign #20 out = in;
-endmodule
+endmodule	//end of module delay
