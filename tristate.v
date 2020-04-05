a816ab0d15460adacc74ac0fa0bda5d4406b3c5a
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 22:19:41 +0530

    comment update

--- tristate.v
+++ tristate.v
@@ -1,6 +1,9 @@
+// Verilog code for tristate
 module tristate(ostream[31:0], enable, istream[31:0]);
+	// inputs and output variable declaration
 	input [31:0] istream;
 	input enable;
 	output [31:0] ostream;
+	// code start
 	assign ostream = enable ? istream : 32'bz;
-endmodule
+endmodule// end of module tristate
