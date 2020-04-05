f86883ec3a7e2ed93ce9fdea2bd917e9c920f896
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 23:35:22 +0530

    Update dec6x64.v

--- dec6x64.v
+++ dec6x64.v
@@ -1,7 +1,10 @@
+// Verilog code
 module dec6x64(out[63:0], in[5:0]);
+	// inputs , outputs and internal variables declaration
 	input [5:0] in;
 	output [63:0] out;
 	wire [7:0] select;
+	// code start
 	dec3x8 dec_main(select[7:0], in[5:3], 1'b1);
 	dec3x8 dec0(out[7:0], in[2:0], select[0]);
 	dec3x8 dec1(out[15:8], in[2:0], select[1]);
@@ -11,5 +14,5 @@ module dec6x64(out[63:0], in[5:0]);
 	dec3x8 dec5(out[47:40], in[2:0], select[5]);
 	dec3x8 dec6(out[55:48], in[2:0], select[6]);
 	dec3x8 dec7(out[63:56], in[2:0], select[7]);
-endmodule
+endmodule	// end of module
 	
