ccffeefa6cf9168e2bcc8bfb037d005234ebb3dd
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 10:58:10 +0530

    comment update

--- mux2x1.v
+++ mux2x1.v
@@ -1,6 +1,9 @@
+// Verilog code for 2X1 Mux
 module mux2x1(out[4:0], in0[4:0], in1[4:0], sel);
+	// inputs and outputs variables declaration
 	input [4:0] in0, in1;
-	output [4:0] out;
 	input sel;
+	output [4:0] out;
+	// Code start
 	assign out = sel ? in0 : in1;
 endmodule
