2c7cf284a29b6efb35c5ca18245e284da22ba90a
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 23:30:37 +0530

    Update decoder_regfile.v

--- decoder_regfile.v
+++ decoder_regfile.v
@@ -1,11 +1,9 @@
+// Verilog code for decoder 
 module decoder_regfile(out[31:0], address[4:0]);
-	input [4:0] address;
-	//integer addrdecimal;
+	// inputs, outputs and internal variables declaration
+ 	input [4:0] address;
 	reg [31:0] temp = 32'b00000000000000000000000000000001;
 	output [31:0] out;
-	/*always @( address )
-		addrdecimal = address;
-	end*/
+	// code start
 	assign out = temp << address;
-endmodule
-				
+endmodule	// end of module decoder
