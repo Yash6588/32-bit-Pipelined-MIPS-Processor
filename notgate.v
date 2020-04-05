5c90b94756f22264ed9a18e95766ac1f6b232b4e
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 11:16:23 +0530

    comment update

--- notgate.v
+++ notgate.v
@@ -1,5 +1,8 @@
+// Verilog code for NOTgate
 module notgate(out,in);
+	// input and output variables declaration
 	input in;
 	output out;
+	// code start
 	assign out = ~in;
-endmodule
+endmodule// end of module NOTgate
