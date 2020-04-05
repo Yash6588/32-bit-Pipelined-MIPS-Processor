4ac2a4b49544c0db24ec9e52ccf527515c6d2e51
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 22:17:18 +0530

    comment update

--- xorgate.v
+++ xorgate.v
@@ -1,5 +1,8 @@
+// Verilog code for XORgate 
 module xorgate(OUT, IN1, IN2);
+	// inputs and output variables declaration
 	input IN1, IN2;
 	output OUT;
+	// code start
 	assign OUT = (IN1 ^ IN2);
-endmodule
+endmodule// end module of XORgate  
