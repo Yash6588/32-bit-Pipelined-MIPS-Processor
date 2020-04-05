0c9a294e374add5024ad6bd76223a4b0e8b1297e
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 23:19:51 +0530

    Update logic_unit.v

--- logic_unit.v
+++ logic_unit.v
@@ -1,9 +1,12 @@
+// Verilog code for logic unit
 module logic_unit(and_out[31:0], or_out[31:0], xor_out[31:0], nor_out[31:0], x[31:0], y[31:0]);
+	// inputs and outputs ports declaration
 	input [31:0] x,y;
 	output [31:0] and_out, or_out, xor_out, nor_out;
+	// code start
 	assign and_out = x & y;
 	assign or_out = x | y;
 	assign xor_out = x ^ y;
 	assign nor_out = ~(x | y);
-endmodule
+endmodule	// end of module logic unit
 	
