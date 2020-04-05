bfa322072f7059a90a497bda0ebc02bd71d0ca24
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 11:12:00 +0530

    comment update

--- serialiserialout.v
+++ serialiserialout.v
@@ -1,8 +1,11 @@
+// Verilog code for Serial in serial out
 module serialiserialout(q,d,clk);
-output reg q;
-input d,clk;
-wire a,b;
+	// inputs, output and internal variables declaration
+	input d,clk;
+	wire a,b;
+	output reg q;
+	// code start
 	D_FlipFlop d1(a, d, clk);
 	D_FlipFlop d2(b, a, clk);
 	D_FlipFlop d3(q, b, clk);
-endmodule
+endmodule// end of module serial in serial out
