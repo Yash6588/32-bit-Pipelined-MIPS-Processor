4a57ef222f8fb268280685fd7e8a305d37568aab
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 20:54:20 +0530

    comment update

--- comparator.v
+++ comparator.v
@@ -1,5 +1,7 @@
+// Verilog module for Comparator
 module comparator(comp_out, in1[4:0], in2[4:0]);
+	//inputs and outputs Variables declaration
 	input [4:0] in1, in2;
 	output comp_out;
 	assign comp_out = ~((in1[4] ^ in2[4]) | (in1[3] ^ in2[3]) | (in1[2] ^ in2[2]) | (in1[1] ^ in2[1]) | (in1[0] ^ in2[0]));
-endmodule
+endmodule //end of module comparator
