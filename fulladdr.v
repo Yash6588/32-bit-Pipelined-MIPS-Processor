02864e6ad33ef138fb80f37f8c46b2e7fd7f190e
Yash6588 <62056483+Yash6588@users.noreply.github.com>
2020-04-05 23:21:41 +0530

    Update fulladdr.v

--- fulladdr.v
+++ fulladdr.v
@@ -1,6 +1,9 @@
+// Verilog code for fulladder
 module fulladdr(sum, Cout, in1, in2, Cin);
+	// inputs and outputs ports declararion
 	input in1, in2, Cin;
 	output sum, Cout;
+	// code start
 	assign sum = in1 ^ in2 ^ Cin;
 	assign Cout = in1&in2 | in2&Cin | in1&Cin;
-endmodule
+endmodule	// end of module fulladder
